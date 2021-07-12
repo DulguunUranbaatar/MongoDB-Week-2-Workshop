acde8ec992dc0927070bf30518449739f5022fd1
DulguunUranbaatar <72236728+DulguunUranbaatar@users.noreply.github.com>
2021-07-12 15:02:25 -0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/nucampsite",
-  facebook: {
-    clientId: "868962507335846",
-    clientSecret: "f78b9256c3aa1242c9f213cfa3b6ab43",
-  },
-};
